7166d0290e9f3b7abbf0c672daf9e54451fa6ba4
Christoffer Gydenius <christgy@axis.com>
2023-12-13 11:51:39 +0100

    WIP
    Change-Id: Idd93581d78e7eb24cc90e8c607e8bc165e925530

--- Day13/task_a.py
+++ Day13/task_a.py
@@ -10,5 +10,49 @@ test_data_path = f"{script_dir}/data_task_a_test"
 
 file = open(test_data_path)
 
+raw_data = ""
+
+matrix_matrix = list()
+
 for l in file:
-    print(l)
+    print(l, end="")
+    if len(l) == 1:
+        matrix_matrix.append(raw_data)
+        raw_data = ""
+    raw_data += l
+matrix_matrix.append(raw_data.lstrip())
+
+# print(matrix_matrix)
+# print(matrix_matrix[0])
+
+def get_row(m, y):
+    print(f">{m[y]}")
+    return m[y]
+
+def get_column(m, x):
+    column = ""
+    for y in range(len(m)):
+        column += m[y][x]
+    return column
+
+def find_reflextion(m):
+    # h
+    # for i in range(len(m[0])):
+    #     print(get_column(m, i))
+
+    # v
+
+    print(m)
+
+    for i in range(len(m)):
+        get_row(m, i)
+        input()
+
+
+
+for m in matrix_matrix:
+    print("-------------")
+    print(m)
+    print("-------------")
+    find_reflextion(m)
+    break
